caba207ec1e8372c67b32e696b426f258c5dfc14
Antonis-Sfikakis-sdi2200178 <126885421+Antonis-Sfikakis-sdi2200178@users.noreply.github.com>
2023-09-03 17:12:46 +0300

    Update hashtables.cpp

--- hashtables.cpp
+++ hashtables.cpp
@@ -7,6 +7,13 @@ using namespace std ;
 #include "hashtable.h"
 
 hash:: hash(){
+  for(int i =0; i < tablesize ; i++) {
+    hashtable[i] = new item; 
+    hashtable[i]->name ="empty" ; 
+    hashtable[i]->drink="empty" ;
+    hashtable[i]->next= NULL; 
+
+  }
    
 }
 
@@ -19,3 +26,53 @@ int hash:: Hash(string key ){
     index=hash % tablesize ; 
     return index ;  
 }  
+
+
+void hash ::AddItem(string name , string drink){
+  int index=Hash(name) ; 
+  if(hashtable[index]=="empty"){
+    hashtable[index]->name=name ; //the position is empty 
+    hashtable[index]->drink=drink;
+  }else{
+    item * ptr = hashtable[index] ;  // create new position
+    item * n = new item ;
+    n->name  = name ;
+    n->drink = drink ; //go to the end of the list 
+    n->next=NULL ;
+    while(ptr->next!=NULL) ptr=ptr->next;
+    ptr->next=n;  
+  }
+} 
+
+
+
+int hash:: NumberOfItems(int index) {
+  int count =0; 
+  if(hashthash(index)->name=="empty") return 0;
+  else{
+    count++;
+    item * ptr = hashtable[index] ; 
+    while (ptr->next != NULL ) {
+      count++;
+      ptr = ptr -> next ; 
+    }
+     
+  }
+  return count ; 
+}
+
+
+void hash :: Printable() {
+  int number 
+  for(int i=0 ; i<tablesize ; ++i) {
+    cout << i << ":" ;
+     if(hashtable[i]->name=="empty") continue; 
+     else{
+        item* ptr = hashtable[i];
+        while((ptr->next)!=NULL ){
+          cout<<prt->name<<" "<<prt->drink<<endl;
+          prt=ptr->next ;
+        }
+     }
+  }
+}
