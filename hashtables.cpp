57ddfc6c8271b8c0b85d49b233543f92d004c598
Antonis-Sfikakis-sdi2200178 <126885421+Antonis-Sfikakis-sdi2200178@users.noreply.github.com>
2023-09-04 18:40:05 +0300

    Update hashtables.cpp

--- hashtables.cpp
+++ hashtables.cpp
@@ -1,10 +1,32 @@
+
+#include "hashtable.h"
 #include <iostream>
 #include <cstdlib>
 using std::string
 using namespace std ;   
 
+class  hash
+{
+    private:        
+        static const int  tablesize=10;//static const means that we can define and use inside the class 
+        struct item{ //list
+            string name ; 
+            string drink ;
+            item *next ;
+        } ; 
+        item * hashtable[tablesize] ; //hash is array with list pointers 
+    
 
-#include "hashtable.h"
+    public:
+        hash();
+        int Hash(string key); 
+        void AddItem(string name , string drink) ; 
+        int NumberOfItems(int index) ; 
+        void PrintTable() ; 
+        void FindDrink(string name) ; //search function 
+        void RemoveItem(string name );
+};
+
 
 hash:: hash(){
   for(int i =0; i < tablesize ; i++) {
@@ -75,4 +97,59 @@ void hash :: Printable() {
         }
      }
   }
+}
+
+void hash::FindDrink(string name)
+{
+  int index= Hash(name) ; 
+  item *ptr = hashtable[index] ;  //search in hash table  
+  if(ptr->drink=="empty") return ;
+  else{
+    while ((ptr->next)!=NULL && !(ptr->name == name)){
+      ptr=ptr->next ;
+    }
+    if(!(ptr->name == name))return ;
+    cout << ptr->drink << endl; 
+  }
+}
+
+
+void hash::RemoveItem(string name ){
+  int index=Hash(name ); 
+  item * ptr = hashtable [index],*p1,*p2;
+  //if the first element is empty then there are no items to remove.
+  if(ptr->nane=="empty"){
+    cout << name << endl ;
+    cout << "NOT FOUND " << endl; 
+  }
+  //case 1 -  only 1 item  and that bucket has a matching name
+  else if(ptr->name== name && ptr->next== NULL) {
+    hashtable[index]->name="empty" ; 
+    hashtable[index]->drink="empty" ; 
+
+  }
+  //case 2 -  match is located in the first bucket but there are more items in the bucket 
+  else if(ptr->name==name ){
+    hashtable[index]=hastable[index]->next;
+    delete ptr ; 
+  }
+  //case 3 -   match found in second or third buckets .
+  else{
+    p1=hashtable[index]->name ; 
+    p2=hashtable[index] ; 
+    while(p1!=NULL && p1->name!=name){
+      p2=p1;
+      p1=p1->next ; 
+    }
+    //case 3.1 - no match
+    if(p1==NULL ) return ;//name not exist
+    else{
+      ptr=p1;//deleting item
+      p1=p1->next;
+      p2->next=p1;
+      delete ptr ;
+    }
+  }
+
+
 }
